f2a0f46400362aedff0f4cce83bb6433222d3bb0
nombre <nombre@jajaja.com>
2021-04-01 13:01:17 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { PaisModule } from './pais/pais.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    PaisModule,        
   ],
   providers: [],
   bootstrap: [AppComponent]
