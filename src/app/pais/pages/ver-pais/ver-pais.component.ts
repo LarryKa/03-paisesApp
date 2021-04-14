8182e72421b8e29c9324280b222db27b4d45ca30
nombre <nombre@jajaja.com>
2021-04-14 13:39:40 -0500

    Listo

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+import { PaisService } from './../../services/pais.service';
+import { Country } from '../../interfaces/paise.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -8,9 +13,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor() { }
+  constructor( 
+    private activatedRoute: ActivatedRoute, 
+    private paisService: PaisService  
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {   
+
+    this.activatedRoute.params
+        .pipe(
+          switchMap( ({id}) => this.paisService.getPaisPorAlpha(id) )
+        )
+        .subscribe(
+          resp => {
+            console.log( resp );
+          }
+        )
+    
   }
 
 }
